5377e8c94ff36093d6a167eb2028401c28209e03
trancu900 <pet.cu20791@gmail.com>
2022-08-19 16:53:15 +0700

    commit

--- src/main/java/com/cg/team_project2/repository/SizeRepository.java
+++ src/main/java/com/cg/team_project2/repository/SizeRepository.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SizeRepository {
     //Tao la Phuong Dev
+    //Tao la Phuong Dev
+    //Tao la Phuong Dev
+    //Tao la Phuong Dev
 }
